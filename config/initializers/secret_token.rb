c18c2903480a859327246072281587b1c35b6918
Murtaza Ali <murtaza7@gmail.com>
2014-12-03 23:18:23 -0500

    Taggable demo app - match all doesnt work.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '2a25bfa8df2dcc944461a81fcdebb2ba98941206bf8b779a2d6a3fad6969f725cf60d426b510d4a37611340c72d5227911af2e556dbb98ca387907a076640156'
